18ad5a9ae543adb9f5366a7431da6e73358fff85
Subhrajit Sahu <subhrajitsahu1990@gmail.com>
2021-05-13 08:30:40 +0530

    Create mainRoute.js

---
+++ resources/JS/mainRoute.js
@@ -0,0 +1,12 @@
+import SplitMyBill from './modules/commonFunctions.js'
+import createGroup from './modules/createGroup.js'
+
+class mainRoute{
+  constructor(){
+      console.log("main route");
+    }
+  
+  getRouteName(){
+      return "route"
+    }
+}
