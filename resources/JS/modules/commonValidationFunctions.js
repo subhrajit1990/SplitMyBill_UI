3aa5e1a4b8cf4118803eac4c677db70f713ec4cd
Subhrajit Sahu <subhrajitsahu1990@gmail.com>
2021-05-13 15:19:55 +0530

    Update commonValidationFunctions.js

--- resources/JS/modules/commonValidationFunctions.js
+++ resources/JS/modules/commonValidationFunctions.js
@@ -6,5 +6,14 @@ class CommonValidationFunctions{
       this.err = s.length ? 0 : 5;
     return this.err;
     }
+    lastname(s){
+        this.err = s.length > 4 ? 0 : 5;
+    return this.err;
+    }
+    email(s) {   
+        let email = (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(s));
+        this.err = a ? 0 : 6;
+    return this.err;
+}
 }
 export default CommonValidationFunctions
