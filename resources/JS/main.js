e46df69b07d9afcdc7d0a7a711108d918a88ee48
Subhrajit Sahu <subhrajitsahu1990@gmail.com>
2021-05-13 08:22:08 +0530

    Update main.js

--- resources/JS/main.js
+++ resources/JS/main.js
@@ -4,10 +4,12 @@ import createGroup from './modules/createGroup.js'
 window.onload = function(){
     console.log("Test Started");
 	
-	function formValidation(){
+	
+}
+
+function formValidation(){
 		const group = new createGroup( 3, 20 );
 		console.log( group.id, group.params ); 
-	}
 }
 
 
