0a1a89366b62a9ee27dcb70b50bba28b744d6c24
Subhrajit Sahu <subhrajitsahu1990@gmail.com>
2021-05-13 14:55:28 +0530

    Update commonValidationEngine.js

--- resources/JS/modules/commonValidationEngine.js
+++ resources/JS/modules/commonValidationEngine.js
@@ -7,7 +7,7 @@ class CommonValidationEngine extends CommonValidationFunctions{
 	}
 	
 	commonValidation(){
-		let nullCheckStatus = super.nullCheck("s")
+		let nullCheckStatus = this.nullCheck("s")
 		console.log("Validation starts with validation engine"+this.form + " :: "+nullCheckStatus);
 	}
 }
