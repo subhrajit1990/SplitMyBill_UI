ff9fedfd9a2da9a805f6d9f3edb150cb9920cbc3
Subhrajit Sahu <subhrajitsahu1990@gmail.com>
2021-05-13 09:47:14 +0530

    Update main.js

--- resources/JS/main.js
+++ resources/JS/main.js
@@ -7,8 +7,21 @@ SMB.bootStrapJSOps = new function(){
 		// doSmething
    	 }
 	formValidation = function(){
+		var form = document.getElementsByClassName("inputType"),
+		form_validator_check = {
+            		lastname: {
+                		verify: [nullcheck,lastname],
+                		message: ["Please enter the last name","Please enter 4 digits last name"]
+            		},
+            		firstname: {
+                		verify: [nullcheck],
+                		message: ["Please enter the first name"]
+            		}
+        	};
+		
 		var mainRt = new mainRoute();
 		console.log(mainRt.getRouteName());
+		mainRt. validateForm(form,form_validator_check);
 	}
 }
 
