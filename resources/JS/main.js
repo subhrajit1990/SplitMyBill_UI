6ad5ec8337904d0d2f103a3885dc2f01e28004e8
Subhrajit Sahu <subhrajitsahu1990@gmail.com>
2021-05-13 08:04:08 +0530

    Update main.js

--- resources/JS/main.js
+++ resources/JS/main.js
@@ -1,29 +1,29 @@
-/*import SplitMyBill from './modules/commonFunctions.js'
-import createGroup from './modules/createGroup.js' */
-class mainFunctionality{
+import SplitMyBill from './modules/commonFunctions.js'
+import createGroup from './modules/createGroup.js'
+/* class mainFunctionality{
 	constructor(){
 		console.log("Executing");
 	}
-/*if (typeof (SMB) == "undefined") {
-    SMB = {}
-}
 
-SMB.bootStrapJSOps = new function(){	
-    this.init = function(){
-	console.log("executing");
-	console.log(SplitMyBill.getName());
-    }
-
-}
-bootStrapJS = SMB.bootStrapJSOps */
-	
 start(){
 	console.log("helo");
-	/*let val = SplitMyBill.getName();
+	let val = SplitMyBill.getName();
 	console.log(val);
 
 	const group = new createGroup( 3, 20 );
-	console.log( group.id, group.params ); */
+	console.log( group.id, group.params ); 
 }
 }
-export default mainFunctionality
+*/
+
+window.onload = function(){
+    console.log("Test Started");
+	
+	const group = new createGroup( 3, 20 );
+	console.log( group.id, group.params ); 
+    
+
+   
+}
+
+
