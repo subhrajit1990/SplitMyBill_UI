9c353b85c802670c076c39a703b2bab4d99c7aeb
Subhrajit Sahu <subhrajitsahu1990@gmail.com>
2021-05-13 15:30:53 +0530

    Update commonValidationEngine.js

--- resources/JS/modules/commonValidationEngine.js
+++ resources/JS/modules/commonValidationEngine.js
@@ -6,9 +6,49 @@ class CommonValidationEngine extends CommonValidationFunctions{
 		this.formValidator = formValidator;
 	}
 	
-	commonValidation(){
-		let nullCheckStatus = this.nullCheck("s")
+	commonValidationFields(){
+		let errorCount = 0,
+        	    element,
+        	    n,
+        	    out;
+        	Object.keys(form).map(function(key, index) {
+            		if(typeof parseInt(key) == "number" && !isNaN(parseInt(key)) ){
+                		element = form[index];
+                		if (!element.hasAttribute("name")) {
+                    			n = element.id, out;
+                		} else if (element.hasAttribute("src")) {
+                    			n = element.src, out;
+                		} else {
+                    			n = element.name, out;
+               			}
+                		if (list[n] && list[n].verify) {
+                    			(list[n].verify).map(function(i,e){
+                   	   			CommonValidatorValid(element, i);
+                        			if (err) {
+                            				list[n].err = err;                       
+                            				console.log("ERROR "+list[n].message[e] || "Error occured");
+                            				errorCount++;
+                        			}
+                    			})
+                		}
+            		}
+        	});
+        	if (errorCount > 0) {
+            	  return false;
+        	}
+		
+		//let nullCheckStatus = this.nullCheck("s")
 		console.log("Validation starts with validation engine"+this.form + " :: "+nullCheckStatus);
+        	return true;
+	
+	}
+	
+	CommonValidatorValid(element, check) {
+    		if (element.type == 'text' || element.type == 'textarea' || element.type == 'email') {
+        		return this.[check](element.value);   
+    		} else {
+        		return this.[check](element);
+    		}
 	}
 }
 
