ee78e2c570a485f0b5669fb1e3e72e82a6f8fa63
Subhrajit Sahu <subhrajitsahu1990@gmail.com>
2021-05-13 09:37:54 +0530

    Update main.js

--- resources/JS/main.js
+++ resources/JS/main.js
@@ -1,31 +1,15 @@
-/*import SplitMyBill from './modules/commonFunctions.js'
-import createGroup from './modules/createGroup.js'
-
-window.onload = function(){
-    console.log("Test Started");
-	
-	
-}
-
-function formValidation(){
-		const group = new createGroup( 3, 20 );
-		console.log( group.id, group.params ); 
-}*/
-
 if (typeof (SMB) == "undefined") {
     SMB = {}
 }
 
 SMB.bootStrapJSOps = new function(){	
     	this.init = function(){
-		//formValidation();
+		// doSmething
    	 }
 	formValidation = function(){
 		var mainRt = new mainRoute();
-	console.log(mainRt.getRouteName());
+		console.log(mainRt.getRouteName());
 	}
-	
-	
 }
 
 bootStrapJS = SMB.bootStrapJSOps
