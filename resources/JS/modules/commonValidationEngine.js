7cff9e1baee4e1d719f2b6643693d4a5bff4c4c5
Subhrajit Sahu <subhrajitsahu1990@gmail.com>
2021-05-13 14:28:37 +0530

    Update commonValidationEngine.js

--- resources/JS/modules/commonValidationEngine.js
+++ resources/JS/modules/commonValidationEngine.js
@@ -1,7 +1,7 @@
 import CommonValidationFunctions from './commonValidationFunctions.js'
 class CommonValidationEngine extends commonValidationFunctions{
 	constructor(){
-		super(); // must call while extending a class, this constructor is from extending class
+		//super(); // must call while extending a class, this constructor is from extending class
 	}
 	
 	commonValidation(form,formValidator){
