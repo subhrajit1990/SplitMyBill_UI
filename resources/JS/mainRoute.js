e23e667b2a55e46b05429dfaa4cb2baf8dddd6a4
Subhrajit Sahu <subhrajitsahu1990@gmail.com>
2021-05-17 15:12:07 +0530

    Update mainRoute.js

--- resources/JS/mainRoute.js
+++ resources/JS/mainRoute.js
@@ -1,5 +1,6 @@
 import CommonValidationEngine from './modules/commonValidationEngine.js'
 import createGroup from './modules/createGroup.js' 
+import addMemberToGroup from './modules/addMemberToGroup.js' 
 
 class MainRoute{
   constructor(){
@@ -38,6 +39,23 @@ class MainRoute{
 		throw new Error("Exception happened during group creation");
 	}
 	return creationGroupStatus;
+  }
+
+  addMemberToGroupRoute(){
+ 	let addMemberStatus = false;
+	try{
+		const payLoad = {"addMembersRequest":{
+			"groupId":"ABC",
+			"groupMembers":[{"accountName":"SSSSSSS","memberAccountNumber":"31312312"},{"accountName":"PPPPPPPPPPPPPP","memberAccountNumber":"23423423432"}],
+		}};
+   		const addMem = new addMemberToGroup( payLoad );
+   		var status = addMem.addMember();
+		console.log( status  );
+		
+	} catch(err){
+		throw new Error("Exception happened during add member to group");
+	}
+	return addMemberStatus;
   }
 }
 window.MainRoute = MainRoute
