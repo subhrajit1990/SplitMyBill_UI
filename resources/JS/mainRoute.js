e910b33b3897d48873fc647768c5bf9e5bd16793
Subhrajit Sahu <subhrajitsahu1990@gmail.com>
2021-05-17 08:22:20 +0530

    Update mainRoute.js

--- resources/JS/mainRoute.js
+++ resources/JS/mainRoute.js
@@ -27,6 +27,35 @@ class MainRoute{
 	try{
    		const group = new createGroup( 3, 20 );
 		console.log( group.id, group.params );
+		
+		
+		
+		const extraParameters = {
+  	
+    headers: {
+    	'Content-Type': 'application/json',
+     	'Accept': 'application/json',
+	    'channel': 'MB',
+	    'masterTxnRefNo': '12121',
+    },
+    method: 'POST',
+    body: JSON.stringify({"createGroupRequest":{
+	"groupName":"ABC",
+	"groupType":"asdasd",
+	"groupMembers":[{"accountName":"SSSSSSS","memberAccountNumber":"31312312"},{"accountName":"PPPPPPPPPPPPPP","memberAccountNumber":"23423423432"}],
+	"creatorAccountNumber":"892749724797"
+}})
+  };
+
+  return fetch('https://sharemybillapi.herokuapp.com/BillManager/expenses/api/createGroup',extraParameters)
+    .then(res => console.log(JSON.stringify(res)))
+    .catch(error => console.log(error))
+		
+		
+		
+		
+		
+		
 	} catch(err){
 		throw new Error("Exception happened during group creation");
 	}
