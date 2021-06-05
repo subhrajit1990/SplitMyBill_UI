9247cbe1181da80da59fea8df14ca22c5d4bfeaf
Subhrajit Sahu <subhrajitsahu1990@gmail.com>
2021-06-05 15:17:23 +0530

    Update main.js

--- resources/JS/main.js
+++ resources/JS/main.js
@@ -19,6 +19,7 @@ SMB.bootStrapJSOps = new function(){
             		navbarCollapsible.classList.remove('navbar-shrink');
         	} else {
 			 navbarCollapsible.classList.add('navbar-shrink');
+		}
 	}
 		
 		this.navigation = function(){
