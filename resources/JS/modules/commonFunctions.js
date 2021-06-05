c274ca9fbcce153341b4266bad1c4d6f81c158ec
Subhrajit Sahu <subhrajitsahu1990@gmail.com>
2021-06-05 17:25:57 +0800

    Add files via upload

---
+++ resources/JS/modules/commonFunctions.js
@@ -0,0 +1,36 @@
+class CommonFunctions{
+	constructor(){
+
+	}
+
+	genMasterTxnRefNo(){
+
+		return new Date().valueOf();
+	}
+
+	getChannel(){
+
+		return 'MB';
+	}
+
+
+	serverCall(uri,extraParameters){
+		const headers = {
+			headers:{
+	    			'Content-Type': 'application/json',
+	     			'Accept': 'application/json',
+		    		'channel': this.getChannel(),
+		    		'masterTxnRefNo': this.genMasterTxnRefNo(),
+			}
+	    	}
+		return fetch('https://sharemybillapi.herokuapp.com/BillManager/'+uri,Object.assign(extraParameters,headers))
+    		.then(response => response.json())
+  		.then(json => {
+    			console.log('parsed json', json) // access json.body here
+  		})
+    		.catch(error => console.log(error))
+
+	}
+}
+ 
+export default CommonFunctions
---
+++ resources/JS/modules/createGroup.js
@@ -0,0 +1,19 @@
+import CommonFunctions from './commonFunctions.js'
+export default class CreateGroup extends CommonFunctions {
+	constructor(params){
+		super();
+		this.params = params;
+	}
+
+	groupCreation(){
+
+	const extraParameters = {
+		
+	    method: 'POST',
+	    body: JSON.stringify(this.params)
+  	};
+
+  	return this.serverCall("expenses/api/createGroup",extraParameters);
+		
+	}
+}
