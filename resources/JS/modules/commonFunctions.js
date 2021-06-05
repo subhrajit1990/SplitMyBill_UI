7c9ecfa1daf3a784e78cdad8a582de6ce42f0214
Subhrajit Sahu <subhrajitsahu1990@gmail.com>
2021-06-05 20:28:40 +0530

    Update commonFunctions.js

--- resources/JS/modules/commonFunctions.js
+++ resources/JS/modules/commonFunctions.js
@@ -14,56 +14,48 @@ class CommonFunctions{
 	}
 
 
-	
-	
-	
-	
 	async  loadJson(url,data) {
-  let response = await fetch(url,data);
-  if (response.status == 200) {
-    return response.json();
-  } else {
-    throw new HttpError(response);
-  }
-}
+	  let response = await fetch(url,data);
+	  if (response.status == 200) {
+	    return response.json();
+	  } else {
+	    throw new "Invalid http status :: "+response.status;
+	  }
+	}
 
-// Ask for a user name until github returns a valid user
-async serverCall(uri,extraParameters) {
+	// Ask for a user name until github returns a valid user
+	async serverCall(uri,extraParameters) {
 
-  let finalFetchData;
-	
-	const headers = {
-			headers:{
-	    			'Content-Type': 'application/json',
-	     			'Accept': 'application/json',
-		    		'channel': this.getChannel(),
-		    		'masterTxnRefNo': this.genMasterTxnRefNo(),
-			}
-	    	}
-	
-  while(true) {
-   
-    try {
-      finalFetchData = await this.loadJson('https://sharemybillapi.herokuapp.com/BillManager/'+uri,Object.assign(extraParameters,headers));
-      break; // no error, exit loop
-    } catch(err) {
-      if ( err.response.status == 404) {
-        // loop continues after the alert
-        console.log( " 404 ");
-      } else {
-        // unknown error, rethrow
-        throw err;
-      }
-    }
-  }
+	  let finalFetchData;
+		
+		const headers = {
+				headers:{
+		    			'Content-Type': 'application/json',
+		     			'Accept': 'application/json',
+			    		'channel': this.getChannel(),
+			    		'masterTxnRefNo': this.genMasterTxnRefNo(),
+				}
+		}
+		
+		while(true) {
+		   
+		    try {
+		      finalFetchData = await this.loadJson('https://sharemybillapi.herokuapp.com/BillManager/'+uri,Object.assign(extraParameters,headers));
+		      break; // no error, exit loop
+		    } catch(err) {
+		      if ( err.response.status == 404) {
+		        // loop continues after the alert
+		        console.log( " 404 ");
+		      } else {
+		        // unknown error, rethrow
+		        throw err;
+		      }
+		    }
+	  	}
 
-
-  console.log(finalFetchData);
-  return finalFetchData;
-}
-
-	
-	
+	console.log(finalFetchData);
+	return finalFetchData;
+	}
 	
 }
  
