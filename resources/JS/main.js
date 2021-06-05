835cd01d60e79fa4c02be81d7e1f13d5c404de8b
Subhrajit Sahu <subhrajitsahu1990@gmail.com>
2021-06-05 15:14:17 +0530

    Update main.js

--- resources/JS/main.js
+++ resources/JS/main.js
@@ -5,7 +5,48 @@ if (typeof (SMB) == "undefined") {
 SMB.bootStrapJSOps = new function(){	
     	this.init = function(){
 		// doSomething
+		 this.navbarShrink();
+		this.navigation();
    	 }
+	
+	this.navbarShrink = function(){
+		console.log("nav bar shrink");
+		const navbarCollapsible = document.body.querySelector('#mainNav');
+        	if (!navbarCollapsible) {
+            		return;
+        	}
+        	if (window.scrollY === 0) {
+            		navbarCollapsible.classList.remove('navbar-shrink');
+        	} else {
+			 navbarCollapsible.classList.add('navbar-shrink');
+	}
+		
+		this.navigation = function{
+		
+		// Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            offset: 72,
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+		
+		}
+	
 	formValidation = function(){
 		// Sample request payload construction -- Starts
 		var form = document.getElementsByClassName("inputType"),
