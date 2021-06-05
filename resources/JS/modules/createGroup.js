4dbd660da069a85fde259a12c0f13ab422789088
Subhrajit Sahu <subhrajitsahu1990@gmail.com>
2021-06-05 19:02:21 +0530

    Update createGroup.js

--- resources/JS/modules/createGroup.js
+++ resources/JS/modules/createGroup.js
@@ -12,7 +12,7 @@ export default class CreateGroup extends CommonFunctions {
 	    method: 'POST',
 	    body: JSON.stringify(this.params)
   	};
-	return this.serverCall().then(response => { return response;});
+	return this.serverCall().then(response => { console.log(response); return response;});
   	//return this.serverCall("expenses/api/createGroup",extraParameters);
 		
 	}
