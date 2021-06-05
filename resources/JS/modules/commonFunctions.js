5b24cba1637ee210f5a31926e0de0c277f325578
Subhrajit Sahu <subhrajitsahu1990@gmail.com>
2021-06-05 18:14:35 +0530

    Update commonFunctions.js

--- resources/JS/modules/commonFunctions.js
+++ resources/JS/modules/commonFunctions.js
@@ -25,10 +25,6 @@ class CommonFunctions{
 	    	}
 		return fetch('https://sharemybillapi.herokuapp.com/BillManager/'+uri,Object.assign(extraParameters,headers))
     		.then(response => response.json())
-  		.then(json => {
-    			console.log('parsed json', json) ;// access json.body here
-			return json;
-  		})
     		.catch(error => console.log(error))
 
 	}
