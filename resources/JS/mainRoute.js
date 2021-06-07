07334355ca6e9653d87576614a984259018bd60d
Subhrajit Sahu <subhrajitsahu1990@gmail.com>
2021-06-07 18:09:08 +0530

    Update mainRoute.js

--- resources/JS/mainRoute.js
+++ resources/JS/mainRoute.js
@@ -1,68 +1,74 @@
 import CommonValidationEngine from './modules/commonValidationEngine.js'
 import createGroup from './modules/createGroup.js' 
 import addMemberToGroup from './modules/addMemberToGroup.js' 
+import cardPlay from './modules/cardPlay.js' 
 
 class MainRoute{
-  constructor(){
+  	constructor(){
       console.log("main route");
     }
   
-  getRouteName(){
+  	getRouteName(){
       return "route"
     }
 	
-   validateForm(form, formValidator){
-	let validateFormflag = false;
-	   try{
-		let validationOps = new CommonValidationEngine(form,formValidator);
-		validateFormflag = validationOps.commonValidationFields();
-   		console.log(JSON.stringify(form) + " :: "+JSON.stringify(formValidator) + " :: "+validateFormflag);
-	   }catch(err){
-	   	throw new Error("Exception happened during form validation");
-	   }
-	return validateFormflag;
-   }
-  async createGroupRoute(payLoad){
- 	let creationGroupRes = "";
-	try{
-   		const group = new createGroup( payLoad );
-   		 creationGroupRes = await group.groupCreation();
-		console.log( creationGroupRes  );
-		
-	} catch(err){
-		throw new Error("Exception happened during group creation");
-	}
-	return creationGroupRes;
-  }
+  	validateForm(form, formValidator){
+		let validateFormflag = false;
+		   try{
+			let validationOps = new CommonValidationEngine(form,formValidator);
+			validateFormflag = validationOps.commonValidationFields();
+	   		console.log(JSON.stringify(form) + " :: "+JSON.stringify(formValidator) + " :: "+validateFormflag);
+		   }catch(err){
+		   	throw new Error("Exception happened during form validation");
+		   }
+			return validateFormflag;
+   	}
+  	async createGroupRoute(payLoad){
+	 	let creationGroupRes = "";
+		try{
+	   		const group = new createGroup( payLoad );
+	   		 creationGroupRes = await group.groupCreation();
+			console.log( creationGroupRes  );
+			
+		} catch(err){
+			throw new Error("Exception happened during group creation");
+		}
+		return creationGroupRes;
+  	}
 
-async fetchGroupRoute(payLoad){
- let fetchGroupRes = "";
-	try{
-   		const group = new createGroup( payLoad );
-   		 fetchGroupRes = await group.fetchGroup();
-		console.log( fetchGroupRes  );
-		
-	} catch(err){
-		throw new Error("Exception happened during group fetch");
+	async fetchGroupRoute(payLoad){
+ 		let fetchGroupRes = "";
+		try{
+	   		const group = new createGroup( payLoad );
+	   		 fetchGroupRes = await group.fetchGroup();
+			console.log( fetchGroupRes  );
+			
+		} catch(err){
+			throw new Error("Exception happened during group fetch");
+		}
+		return fetchGroupRes;
 	}
-	return fetchGroupRes;
-}
+	
+  	addMemberToGroupRoute(){
+	 	let addMemberStatus = false;
+		try{
+			const payLoad = {"addMembersRequest":{
+				"groupId":"ABC",
+				"groupMembers":[{"accountName":"SSSSSSS","memberAccountNumber":"31312312"},{"accountName":"PPPPPPPPPPPPPP","memberAccountNumber":"23423423432"}],
+			}};
+	   		const addMem = new addMemberToGroup( payLoad );
+	   		var status = addMem.addMember();
+			console.log( status  );
+			
+		} catch(err){
+			throw new Error("Exception happened during add member to group");
+		}
+		return addMemberStatus;
+  	}
 
-  addMemberToGroupRoute(){
- 	let addMemberStatus = false;
-	try{
-		const payLoad = {"addMembersRequest":{
-			"groupId":"ABC",
-			"groupMembers":[{"accountName":"SSSSSSS","memberAccountNumber":"31312312"},{"accountName":"PPPPPPPPPPPPPP","memberAccountNumber":"23423423432"}],
-		}};
-   		const addMem = new addMemberToGroup( payLoad );
-   		var status = addMem.addMember();
-		console.log( status  );
-		
-	} catch(err){
-		throw new Error("Exception happened during add member to group");
+	gamePlay("timer", 180){
+		let counter = new cardPlay(); // expects in seconds
+		counter.initiateGame(); 
 	}
-	return addMemberStatus;
-  }
 }
 window.MainRoute = MainRoute
