d62391c2c30ab809b4c9af69d412d014da719f76
Subhrajit Sahu <subhrajitsahu1990@gmail.com>
2021-06-10 07:30:52 +0530

    Update cardPlay.js

--- resources/JS/modules/cardPlay.js
+++ resources/JS/modules/cardPlay.js
@@ -1,3 +1,4 @@
+import CommonFunctions from './commonFunctions.js'
 export default class CardPlay {
 	
 	constructor(initiateTimer,time){
@@ -19,7 +20,6 @@ export default class CardPlay {
 
 	}
 
-
 	generatePlayGround() {
 		console.log("1. generating play ground");
 		let cardItemList = this.shuffleCards(this.cardSymbols);
@@ -28,21 +28,20 @@ export default class CardPlay {
 		restart.onclick = function() {
     			self.playAgain();
 		};
-    		cardItemList.map(function (cardClassName, index) {
-        
-        		let cardDeck = document.querySelector(".playGround");
-        		let cardItem = document.createElement("li");
-        		cardItem.setAttribute('id', index);
-        		cardItem.setAttribute('name', cardClassName);
-        		cardItem.classList.add("card");
-   			cardItem.onclick = function() {
-    				self.playGame(this);
-			};
-        		let symbolsItem = document.createElement("i");
-        		symbolsItem.classList.add("fa");
-        		symbolsItem.classList.add(cardClassName);
-        		cardItem.appendChild(symbolsItem);
-        		cardDeck.appendChild(cardItem);
+    	cardItemList.map(function (cardClassName, index) {        
+        	let cardDeck = document.querySelector(".playGround"),
+        	cardItem = document.createElement("li");
+        	cardItem.setAttribute('id', index);
+        	cardItem.setAttribute('name', cardClassName);
+        	cardItem.classList.add("card");
+   			ardItem.onclick = function() {
+    			self.playGame(this);
+			 };
+        	let symbolsItem = document.createElement("i");
+        	symbolsItem.classList.add("fa");
+        	symbolsItem.classList.add(cardClassName);
+        	cardItem.appendChild(symbolsItem);
+        	cardDeck.appendChild(cardItem);
    		});
 		
 	}
@@ -50,8 +49,8 @@ export default class CardPlay {
 	shuffleCards(cards){
 		console.log("2. shuffling");
 		var currentIndex = cards.length,
-        			   temporaryValue, 
-				   randomIndex;
+        		temporaryValue, 
+				randomIndex;
 
     		while (currentIndex !== 0) {
         		randomIndex = Math.floor(Math.random() * currentIndex);
@@ -80,17 +79,17 @@ export default class CardPlay {
         	this.chooseCard();
     	} else {
         	let item = {
-            		id: tempCard.getAttribute('id'),
-            		name: tempCard.getAttribute('name')
+            	id: tempCard.getAttribute('id'),
+            	name: tempCard.getAttribute('name')
         	};
 
         	if (this.checkForMatch(item)) {
            		tempCard.classList.add('match');
-            		tempCard.removeAttribute('onclick');
-            		this.visibleCard.classList.add('match');
-            		this.visibleCard.removeAttribute('onclick');
-            		this.matchCardnumber += 1;
-            		this.gameOver();
+            	tempCard.removeAttribute('onclick');
+            	this.visibleCard.classList.add('match');
+            	this.visibleCard.removeAttribute('onclick');
+            	this.matchCardnumber += 1;
+            	this.gameOver();
        		}
         	this.firstCard = tempCard;
         	this.secondCard = this.visibleCard;
@@ -99,20 +98,20 @@ export default class CardPlay {
     	}
 	}
 
-    	gameOver(){
+    gameOver(){
 		console.log("game over");
-		 if (this.matchCardnumber == this.numberOfCards) {
-			this.saveMatchScore();
-        		let modal = document.querySelector('.popup');
-        		let close = document.querySelector('.close');
-        		document.querySelector("#moves").textContent = self.movescount;
-        		document.querySelector('#timer').textContent = (this.timercount).getTimeValues().toString();
-        		modal.style.display = "block";
-        		close.onclick = function () {
-            			modal.style.display = "none";
-            			location.reload();
-        		}	
-    		}
+        if (this.matchCardnumber == this.numberOfCards) {
+            this.saveMatchScore();
+        	let modal = document.querySelector('.popup');
+        	let close = document.querySelector('.close');
+        	document.querySelector("#moves").textContent = self.movescount;
+        	document.querySelector('#timer').textContent = (this.timercount).getTimeValues().toString();
+        	modal.style.display = "block";
+        	close.onclick = function () {
+            	modal.style.display = "none";
+            	location.reload();
+        	}	
+    	}
 
 	}
 
@@ -120,23 +119,24 @@ export default class CardPlay {
 		let self = this;
 		console.log("clear the ground");
 		setTimeout(() => {
-        		if (self.firstCard) {
-            			self.firstCard.classList.remove('open','show');
-            			self.firstCard = null;
-        		}
-       			if (self.secondCard) {
-            			self.secondCard.classList.remove('open','show');
-            			self.secondCard = null;
-        		}
-    		}, 1000);
+        	if (self.firstCard) {
+            	self.firstCard.classList.remove('open','show');
+            	self.firstCard = null;
+        	}
+       		if (self.secondCard) {
+            	self.secondCard.classList.remove('open','show');
+            	self.secondCard = null;
+        	}
+    	}, 1000);
 	}
+
 	checkForMatch(item){
 		console.log("matched");
 		let card = {
-        		id: this.visibleCard.getAttribute('id'),
-        		name: this.visibleCard.getAttribute('name'),
-        		cardIsOpen: this.visibleCard.classList.contains('open')
-    		};
+        	id: this.visibleCard.getAttribute('id'),
+        	name: this.visibleCard.getAttribute('name'),
+        	cardIsOpen: this.visibleCard.classList.contains('open')
+    	};
     	return (item.name === card.name && item.id !== card.id && card.cardIsOpen);
 	};
 
@@ -145,42 +145,63 @@ export default class CardPlay {
 		let movesContainer = document.querySelector('.moves');
     		movesContainer.textContent = this.movescount;		
 	}
+
 	playAgain(){
 		console.log("once more");
 		location.reload();
 		
 	}
-       timer() {
-	        let self = this;
+
+    timer() {
+        let self = this;
 		console.log("timer");
-    		(this.timercount).start();
-    		(this.timercount).addEventListener('secondsUpdated', function (e) {
-        		let basicUsagetimer = document.querySelector('#basicUsage');
-        		basicUsagetimer.textContent = (self.timercount).getTimeValues().toString();
-    		});
+    	(this.timercount).start();
+    	(this.timercount).addEventListener('secondsUpdated', function (e) {
+        	let basicUsagetimer = document.querySelector('#basicUsage');
+        	basicUsagetimer.textContent = (self.timercount).getTimeValues().toString();
+    	});
 	};
-	fetchCards(){	
+
+	async fetchCards(){	
 		console.log("0.Service call to fetch the list of cards");
-		//temporary data >> STARTS
-		let fetchCardsList = ["fa-snapchat", "fa-glide", "fa-braille","fa-viadeo","fa-blind","fa-gitlab", "fa-bluetooth", "fa-shopping-bag"];
-		//temporary data >> ENDS
+        /* 
+        fetch the card icons from external service and hard code few icons
+        "fa-snapchat", "fa-glide", "fa-braille","fa-viadeo","fa-blind","fa-gitlab", "fa-bluetooth", "fa-shopping-bag"
+         */
+       
+        let fethData = await this.serverCall("Msc/api/fetchCards",{method: 'GET'}).then(response => {
+            console.log(response); 
+            return response;
+         });
+      
+        let fetchCardsList = [];
+        if(fethData.ResponseHeader.ResponseCode === "0"){
+
+            (fetchCardsRes.gameCardListResponse.gameCardResponse).map((ele,index) => {
+                fetchCardsList.push(ele.cardIcon);
+
+            });
+
+        }
 		this.numberOfCards=(fetchCardsList.length);
 		let toBeSuffleCards = this.duplicateCards(fetchCardsList,(this.numberOfCards)*2);
 		return toBeSuffleCards;
 	};
+
 	duplicateCards(pattern, count){
 		let result = [];
    		if (["number", "string"].includes(typeof pattern)) {
         		result = new Array(count);
         		result.fill(pattern);
-    		}else if (pattern instanceof Array) {
-			pattern.map(function(e,i){
-				result = result.concat(pattern);
-			})
-        		result = result.slice(0, count);
-    		}
-    		return result;
+    	}else if (pattern instanceof Array) {
+    		pattern.map(function(e,i){
+    			result = result.concat(pattern);
+    		})
+        	result = result.slice(0, count);
+    	}
+    	return result;
 	};
+    
 	saveMatchScore(){
 		console.log("API call to save the match score");
 	};
