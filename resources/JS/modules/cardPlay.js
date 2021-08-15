11dcacb2c4176969e3fb43be646d5093fa293f72
Subhrajit Sahu <subhrajitsahu1990@gmail.com>
2021-08-15 12:43:55 +0530

    Update cardPlay.js

--- resources/JS/modules/cardPlay.js
+++ resources/JS/modules/cardPlay.js
@@ -182,7 +182,7 @@ export default class CardPlay extends CommonFunctions {
             return response;
          }); */
 		
-	fethData = 	{
+	let fethData = 	{
   "ResponseHeader": {
     "ResponseCode": "0",
     "httpStatus": "OK"
