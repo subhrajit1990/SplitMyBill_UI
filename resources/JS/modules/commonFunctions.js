ce9b154580ff8e9c294922e043a33e3553ca8d67
Subhrajit Sahu <subhrajitsahu1990@gmail.com>
2021-12-26 15:04:41 +0530

    Update commonFunctions.js

--- resources/JS/modules/commonFunctions.js
+++ resources/JS/modules/commonFunctions.js
@@ -35,11 +35,8 @@ class CommonFunctions{
 			    		'channel': this.getChannel(),
 			    		'masterTxnRefNo': this.genMasterTxnRefNo(),
 					'Authorization':'Basic YWRtaW46cGFzc3dvcmQ=',
-					"Access-Control-Allow-Origin": "*",
-      					"Access-Control-Allow-Credentials": "true",
-      					"Access-Control-Allow-Methods": "GET,HEAD,OPTIONS,POST,PUT",
-      					"Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-				}
+				},
+			mode: 'no-cors',
 		}
 
 		let absPath = "";
