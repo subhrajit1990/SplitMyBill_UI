5cdbf8df30628b7788f5c79cb217811b6d8cf77a
Subhrajit Sahu <subhrajitsahu1990@gmail.com>
2022-02-23 21:04:13 +0530

    Update main.js

--- resources/JS/main.js
+++ resources/JS/main.js
@@ -229,6 +229,28 @@ SMB.bootStrapJSOps = new function(){
  			tempGroupForm.style.display = "block";
   		}
 		
+		let speech = true;
+        window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+  
+        const recognition = new SpeechRecognition();
+        recognition.interimResults = true;
+        const words = document.querySelector('.words');
+        words.appendChildpparagraph;
+  
+        recognition.addEventListener('result', e => {
+            const transcript = Array.from(e.results)
+                .map(result => result[0])
+                .map(result => result.transcript)
+                .join('')
+  
+            document.getElementById("paragraph).innerHTML = transcript;
+            console.log(transcript);
+        });
+          
+        if (speech == true) {
+            recognition.start();
+            recognition.addEventListener('end', recognition.start);
+        }
 		
 	}
 }
